e783d0c661c8b5c5b3c7010f10826589c0871471
Kananelo688 <kchabeli688@gmail.com>
2025-02-04 20:17:10 +0200

    completed home and about pages

--- app.js
+++ app.js
@@ -23,34 +23,6 @@
 })(jQuery, document, window);
 
 
-
-function joinCommunityEventHandler(){
-
-	/**Display the content of the registration form */
-	if(this.id == "join-community-button"){
-		document.getElementById("community-form").classList.add("animate__fadeIn","show");
-	}else if(this.id == "prayer-request-button"){
-		document.getElementById("service-request-form").classList.add("animate__fadeIn","show");
-	}else if(this.id == "nnena-see-more"){
-		
-	}else if(this.id == "mampho-see-more"){
-		document.getElementById("mampho-more").classList.add("show","animate__fadeIn");
-		this.style.display = "none";
-	}
-	
-
-
-}
-
-function joinChurchEvent(){
-	this.parentNode.nextElementSibling.classList.add("animate__fadeIn","show");
-}
-
-function eventCancelHandler(){
-	this.parentNode.parentNode.parentNode.classList.remove("show");
-	this.parentNode.parentNode.parentNode.classList.replace("animate__fadeIn","animate__fadeOut");
-
-}
 /**
  * Handles click events fired by the buttons in the About PAGE of the website
  */
@@ -60,12 +32,15 @@ function sectionButtonEventHandler() {
 
     if (this.id.endsWith('-open-button')) {
         content.classList.add("show", "animate__fadeIn");
-        content.classList.remove("animate__fadeOut");
+        
+		content.classList.replace("animate__fadeOut","animate__fadeIn");
+	
         this.lastElementChild.src = "images/drop_up.webp";
-        this.id = this.id.replace('-open-button', '-close-button');
+        
+		this.id = this.id.replace('-open-button', '-close-button');
     } else {
         content.classList.remove("show", "animate__fadeIn");
-        content.classList.add("animate__fadeOut");
+        // content.classList.replace("animate__fadeIn","animate__fadeOut");
         this.lastElementChild.src = "images/dropdown.png";
         this.id = this.id.replace('-close-button', '-open-button');
     }
@@ -77,66 +52,56 @@ function main(){
 
 	document.querySelectorAll(".section-bar").forEach(button=>{
 		button.addEventListener('click',sectionButtonEventHandler);
-	})
-
-	// document.getElementById("about-open-button").addEventListener('click',()=>{
-	// 	document.getElementById("about-content").classList.add("show","animate__fadeIn");
-	// });
-
-	/**Add Action Listener for the "JOIN OUR COMMUNITIES BUTTON" */
-	document.getElementById("join-community-button").addEventListener('click',joinCommunityEventHandler);
-
-	/**Add Action Listener for the REQUEST PRAYER BUTTON */
-	document.getElementById("prayer-request-button").addEventListener('click',joinCommunityEventHandler);
-
-	/**add event handlerto pastors' 'read-more' buttons */
-	document.getElementById('nnena-see-more').addEventListener('click',joinCommunityEventHandler);
-	document.getElementById('mampho-see-more').addEventListener('click',joinCommunityEventHandler);
-
-
-	/**Event Handler for Community Cancel Button*/
-	document.getElementById("community-cancel-button").addEventListener('click',()=>{
-		/**Remove the show class from this element's classList */
-		//document.getElementById("community-form").classList.remove("show");
-		/**Replace fadeIn with fadeOut on button click*/
-		document.getElementById("community-form").classList.replace("animate__fadeIn","animate__fadeOut");
-
-		document.getElementById("community-form").classList.remove("show");
 	});
 
-	document.getElementById("nnena-see-less").addEventListener('click',()=>{
-		document.getElementById('nnena-more').classList.remove('show');
-		document.getElementById('nnena-see-more').style.display= "block";
+	/**Event Handler for the REQUEST OUR SERVICE */
+	document.getElementById("prayer-request-button").addEventListener('click',()=>{
+		document.getElementById("service-request-form").classList.add("show","animate__fadeIn");
+		document.getElementById("service-request-form").classList.replace("animate__fadeOut","animate__fadeIn");
+		document.getElementById("prayer-request-button").style.display = "none";
 	});
 
-	document.getElementById("mampho-see-less").addEventListener('click',()=>{
-		document.getElementById('mampho-more').classList.remove('show');
-		document.getElementById('mampho-see-more').style.display= "block";
-	});
-
-	document.getElementById("nnena-see-more").addEventListener('click',()=>{	
-		document.getElementById("nnena-more").classList.add("show","animate__fadeIn");
-		this.style.display = "none";
-		alert("hee!");
-	})
-
-
-	/**Action Listener for Service request cancel button */
+	/**Event Handler for the SERVICE REQUEST FORM Cancel Button */
 	document.getElementById("service-request-cancel-button").addEventListener('click',()=>{
-		document.getElementById("service-request-form").classList.replace("animate__fadeIn","animate__fadeOut");
 		document.getElementById("service-request-form").classList.remove("show");
+		document.getElementById("service-request-form").classList.replace("animate__fadeIn","animate__fadeOut");
+		document.getElementById("prayer-request-button").style.display = "inline";
 	});
 
-	/**Action Lister to Join Event Buttons */
-	document.querySelectorAll(".join-event-button").forEach(button=>{
-		button.addEventListener('click',joinChurchEvent);
+	/**Event Handler for the JOIN COMMUNITY BUTTON */
+	document.getElementById("join-community-button").addEventListener('click',()=>{
+		document.getElementById("community-form").classList.add("show","animate__fadeIn");
+		document.getElementById("community-form").classList.replace("animate__fadeOut","animate__fadeIn");
+		document.getElementById("join-community-button").style.display = "none";
 	});
 
-	/**Action Listener for Event Cancel Buttons */
+	/**Event Handler for the JOIN COMMUNITY BUTTON CANCEL Button */
+	document.getElementById("community-cancel-button").addEventListener('click',()=>{
+		document.getElementById("community-form").classList.remove("show");
+		document.getElementById("community-form").classList.replace("animate__fadeIn","animate__fadeOut");
+		document.getElementById("join-community-button").style.display = "inline";
+	});
+
+	/**Event Handler for JOIN EVENT BUTTONS*/
+	document.querySelectorAll(".join-event-buttons").forEach(button=>{
+		button.addEventListener('click',()=>{
+			button.parentNode.nextElementSibling.classList.add("show","animate__fadeIn");
+			button.parentNode.nextElementSibling.classList.replace("animate__fadeOut","animate__fadeIn");
+			button.parentNode.style.display = "none"
+		});
+	});
+
+	/**Event Handler for JOIN EVENT CANCEL BUTTONS */
 	document.querySelectorAll(".event-registration-cancel-buttons").forEach(button=>{
-		button.addEventListener('click',eventCancelHandler);
-	});
-	
+		button.addEventListener('click',()=>{
+			button.parentNode.parentNode.parentNode.classList.remove("show");
+			button.parentNode.parentNode.parentNode.classList.replace("animate__fadeIn","animate__fadeOut");
+			button.parentNode.parentNode.parentNode.previousElementSibling.style.display='flex';
+
+
+		});
+	})
+
 }
 
 main();
